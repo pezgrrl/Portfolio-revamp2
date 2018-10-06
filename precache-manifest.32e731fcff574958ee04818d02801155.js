226edbe7c12191fae515d10cbc57bd84a4ab0381
Jill Morimoto <jillmorimoto@gmail.com>
2018-10-06 14:21:35 -0700

    Updates

---
+++ precache-manifest.32e731fcff574958ee04818d02801155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38516731bc2df75a039d",
+    "url": "/Portfolio-revamp2/static/js/runtime~main.38516731.js"
+  },
+  {
+    "revision": "acaf9a1dd06cfc514baf",
+    "url": "/Portfolio-revamp2/static/js/main.acaf9a1d.chunk.js"
+  },
+  {
+    "revision": "7973823ea2d5acac0368",
+    "url": "/Portfolio-revamp2/static/js/1.7973823e.chunk.js"
+  },
+  {
+    "revision": "acaf9a1dd06cfc514baf",
+    "url": "/Portfolio-revamp2/static/css/main.b66217eb.chunk.css"
+  },
+  {
+    "revision": "7973823ea2d5acac0368",
+    "url": "/Portfolio-revamp2/static/css/1.31a7cf3d.chunk.css"
+  },
+  {
+    "revision": "74deb81a5ac941635ea7bc841a40e7e4",
+    "url": "/Portfolio-revamp2/index.html"
+  }
+];
